96984a277d474099bca3cee43d2b7143de596b56
David Enke <david.enke@zalari.de>
2018-09-12 12:52:29 +0200

    Added type check
    to add double quotes to strings only, or better say: to vars which are not numeric, boolean nor `null`

--- init/jsonenv.sh
+++ init/jsonenv.sh
@@ -7,7 +7,7 @@
 # the basic idea is to use prefixed environment variables which have to follow certain conventions:
 # * will be lowercased
 # * underscores are replaced to dots (like json path) for nesting
-# this creates a jq filter expression: https://jqplay.org/s/EHP-iWpiZO 
+# this creates a jq filter expression: https://jqplay.org/s/EHP-iWpiZO
 
 ENV_PREFIX=$1
 PATH_JSON=$2
@@ -38,12 +38,19 @@ do
     # https://stackoverflow.com/a/2264537/1146207
     # path_name=$(echo "${path_name}" | awk '{print tolower($0)}')
 
+    # add double quotes to strings
+    _env_type_check='^([+-]?[0-9]+([.][0-9]+)?)|true|false|null$'
+    if ! [[ ${!env_name} =~ ${_env_type_check} ]];
+        then env_value="\"${!env_name}\"";
+        else env_value=${!env_name}
+    fi
+
     # append to filter
     # https://stackoverflow.com/a/1951523/1146207
-    jq_filters+=("${path_name} = \"${!env_name}\"")
+    jq_filters+=("${path_name} = ${env_value}")
 
     # debug output
-    echo "${env_name} > ${path_name} = ${!env_name}"
+    echo "${env_name} > ${path_name} = ${env_value}"
 done
 
 # join filters, remove delimiter up front
